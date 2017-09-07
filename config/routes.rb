eeecd6cfee1ea11ade541c3ebbc7028051646b83
shiotomo <t.shiota16@gmail.com>
2017-09-07 10:00:33 +0000

    Add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :welcomes
+
+  root to: 'welcomes#index'
+  # devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    # passwords: 'users/passwords'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
