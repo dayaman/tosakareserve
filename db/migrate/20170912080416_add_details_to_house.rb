b86fa18805b6ad423f94dd82cbbdc91564912e30
shiotomo <t.shiota16@gmail.com>
2017-09-27 16:31:25 +0900

    データベースの修正と機能の実装

--- db/migrate/20170912080416_add_details_to_house.rb
+++
@@ -1,6 +0,0 @@
-class AddDetailsToHouse < ActiveRecord::Migration[5.1]
-  def change
-    add_column :houses, :amazon, :text
-    add_column :houses, :img, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170927042936) do
+ActiveRecord::Schema.define(version: 20170927064807) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,23 +28,11 @@ ActiveRecord::Schema.define(version: 20170927042936) do
     t.index ["name"], name: "index_admin_users_on_name", unique: true
   end
 
-  create_table "houses", force: :cascade do |t|
+  create_table "owners", force: :cascade do |t|
     t.string "name"
     t.integer "capacity"
     t.text "news"
     t.string "owner"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.text "amazon"
-    t.string "img"
-  end
-
-  create_table "reserves", force: :cascade do |t|
-    t.string "house"
-    t.string "user"
-    t.date "check_in"
-    t.date "check_out"
-    t.text "omiyage"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
